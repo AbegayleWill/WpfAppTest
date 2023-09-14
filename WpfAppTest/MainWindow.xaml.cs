14976435a0ac312619d97fe0766074b542a62d09
Douglas Byfield <douglas.byfield@mcsystems.com>
2023-09-14 13:42:28 -0500

    collecting coins work

--- WpfAppTest/MainWindow.xaml.cs
+++ WpfAppTest/MainWindow.xaml.cs
@@ -1,25 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfAppTest
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private int score = 0; // Track the score
 
         public MainWindow()
         {
@@ -30,13 +19,9 @@ namespace WpfAppTest
         {
             double moveAmount = 10.0; // Amount to move Pac-Man
 
-
-
             // Get current position
             double currentLeft = Canvas.GetLeft(pacman);
             double currentTop = Canvas.GetTop(pacman);
-
-
 
             switch (e.Key)
             {
@@ -53,21 +38,41 @@ namespace WpfAppTest
                     Canvas.SetLeft(pacman, currentLeft + moveAmount);
                     break;
             }
+
+            CheckForCollisions(); // Check for collisions after moving Pac-Man
         }
 
+        private void CheckForCollisions()
+        {
+            double pacmanMargin = 5; // Adjust this value as needed for Pac-Man
+            double pelletMargin = 2.5; // Adjust this value as needed for Pellets
 
+            Rect pacmanRect = new Rect(Canvas.GetLeft(pacman) + pacmanMargin, Canvas.GetTop(pacman) + pacmanMargin, pacman.Width - 2 * pacmanMargin, pacman.Height - 2 * pacmanMargin);
+
+            foreach (UIElement child in MyCanvas.Children)
+            {
+                if (child is Image && (child as Image).Tag?.ToString() == "Pellet" && child.Visibility == Visibility.Visible)
+                {
+                    Rect pelletRect = new Rect(Canvas.GetLeft(child) + pelletMargin, Canvas.GetTop(child) + pelletMargin, (child as Image).Width - 2 * pelletMargin, (child as Image).Height - 2 * pelletMargin);
+
+                    if (pacmanRect.IntersectsWith(pelletRect))
+                    {
+                        child.Visibility = Visibility.Collapsed; // Hide the pellet
+                        score++; // Increase the score
+                        scoreText.Content = "Score: " + score; // Update the score label
+                    }
+                }
+            }
+        }
 
         private void StartGame_Click(object sender, RoutedEventArgs e)
         {
             // Show the game canvas and hide the start button
             MyCanvas.Visibility = Visibility.Visible;
-            StartButton.Visibility = Visibility.Collapsed;////
+            StartButton.Visibility = Visibility.Collapsed;
 
             // Focus on the canvas to allow key events
             MyCanvas.Focus();
         }
-
-
     }
 }
-
