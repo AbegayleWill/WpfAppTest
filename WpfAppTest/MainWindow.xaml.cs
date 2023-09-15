81c6160b56ce2ae0d612be196be075102f2fcfb2
Douglas Byfield <douglas.byfield@mcsystems.com>
2023-09-14 22:05:59 -0500

    enemy one moving

--- WpfAppTest/MainWindow.xaml.cs
+++ WpfAppTest/MainWindow.xaml.cs
@@ -11,15 +11,85 @@ namespace WpfAppTest
     public partial class MainWindow : Window
     {
         private int score = 0; // Track the score
+        private bool isGameEnded = false;
+        private double pacmanOriginalLeft;
+        private double pacmanOriginalTop;
+        private int lives = 3; // Start with 3 lives
+
 
         private Random random = new Random();
         private DispatcherTimer enemyMoveTimer = new DispatcherTimer();
+
         public MainWindow()
         {
             InitializeComponent();
 
-        }
+            // Store Pac-Man's original position
+            pacmanOriginalLeft = Canvas.GetLeft(pacman);
+            pacmanOriginalTop = Canvas.GetTop(pacman);
 
+            enemyMoveTimer.Interval = TimeSpan.FromMilliseconds(500); // Adjust the interval as needed
+            enemyMoveTimer.Tick += EnemyMoveTimer_Tick;
+            enemyMoveTimer.Start();
+        }
+        private void EnemyMoveTimer_Tick(object sender, EventArgs e)
+        {
+            foreach (UIElement child in MyCanvas.Children)
+            {
+                if (child is Image && (child as Image).Tag?.ToString() == "Enemy")
+                {
+                    MoveEnemyRandomly(child as Image);
+                }
+            }
+        }
+        private void MoveEnemyRandomly(Image enemy)
+        {
+            double moveAmount = 10.0; // Amount to move the enemy
+            double currentLeft = Canvas.GetLeft(enemy);
+            double currentTop = Canvas.GetTop(enemy);
+            double newLeft = currentLeft;
+            double newTop = currentTop;
+
+            int direction = random.Next(4); // 0: Up, 1: Down, 2: Left, 3: Right
+
+            switch (direction)
+            {
+                case 0:
+                    newTop -= moveAmount;
+                    break;
+                case 1:
+                    newTop += moveAmount;
+                    break;
+                case 2:
+                    newLeft -= moveAmount;
+                    break;
+                case 3:
+                    newLeft += moveAmount;
+                    break;
+            }
+            // Check for collisions with maze blocks
+            Rect enemyRect = new Rect(newLeft, newTop, enemy.Width, enemy.Height);
+            bool collisionDetected = false;
+
+            foreach (UIElement child in MyCanvas.Children)
+            {
+                if (child is Rectangle && (child as Rectangle).Tag?.ToString() == "MazeBlock")
+                {
+                    Rect mazeBlockRect = new Rect(Canvas.GetLeft(child), Canvas.GetTop(child), (child as Rectangle).Width, (child as Rectangle).Height);
+                    if (enemyRect.IntersectsWith(mazeBlockRect))
+                    {
+                        collisionDetected = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!collisionDetected)
+            {
+                Canvas.SetLeft(enemy, newLeft);
+                Canvas.SetTop(enemy, newTop);
+            }
+        }
         private void Canvas_KeyDown(object sender, KeyEventArgs e)
         {
             double moveAmount = 10.0; // Amount to move Pac-Man
@@ -28,7 +98,7 @@ namespace WpfAppTest
             double currentLeft = Canvas.GetLeft(pacman);
             double currentTop = Canvas.GetTop(pacman);
 
-           
+
             double newLeft = currentLeft;
             double newTop = currentTop;
 
@@ -64,10 +134,23 @@ namespace WpfAppTest
             }
         }
 
+        private bool AreAllPelletsCollected()
+        {
+            foreach (UIElement child in MyCanvas.Children)
+            {
+                if (child is Image && (child as Image).Tag?.ToString() == "Pellet" && child.Visibility == Visibility.Visible)
+                {
+                    return false; // At least one pellet is still visible
+                }
+            }
+            return true; // All pellets are collected
+        }
+
         private void CheckForCollisions()
         {
             double pacmanMargin = 5; // Adjust this value as needed for Pac-Man
             double pelletMargin = 2.5; // Adjust this value as needed for Pellets
+            if (isGameEnded) return; // Exit the method if the game has ended
 
             Rect pacmanRect = new Rect(Canvas.GetLeft(pacman) + pacmanMargin, Canvas.GetTop(pacman) + pacmanMargin, pacman.Width - 2 * pacmanMargin, pacman.Height - 2 * pacmanMargin);
 
@@ -84,6 +167,24 @@ namespace WpfAppTest
                         scoreText.Content = "Score: " + score; // Update the score label
                     }
                 }
+                if (child is Image && (child as Image).Tag?.ToString() == "Enemy")
+                {
+                    Rect enemyRect = new Rect(Canvas.GetLeft(child), Canvas.GetTop(child), (child as Image).Width, (child as Image).Height);
+                    if (pacmanRect.IntersectsWith(enemyRect))
+                    {
+                        lives--; // Decrease the number of lives
+                        if (lives <= 0)
+                        {
+                            EndGame("Game Over! You were caught by an enemy.");
+                        }
+                        else
+                        {
+                            MessageBox.Show($"You were caught by an enemy! Lives remaining: {lives}");
+                            Canvas.SetLeft(pacman, pacmanOriginalLeft); // Reset Pac-Man's position
+                            Canvas.SetTop(pacman, pacmanOriginalTop);
+                        }
+                    }
+                }
                 // Check for collisions with maze blocks
                 if (child is Rectangle && (child as Rectangle).Tag?.ToString() == "MazeBlock")
                 {
@@ -94,7 +195,40 @@ namespace WpfAppTest
                         MessageBox.Show("Pac-Man collided with the maze!");
                     }
                 }
+                if (AreAllPelletsCollected())
+                {
+                    EndGame("Congratulations! You collected all the pellets!");
+                }
             }
+        }
+
+        private void ResetGame()
+        {
+            // Reset the score
+            score = 0;
+            scoreText.Content = "Score: " + score;
+
+            // Reset the visibility of all pellets
+            foreach (UIElement child in MyCanvas.Children)
+            {
+                if (child is Image && (child as Image).Tag?.ToString() == "Pellet")
+                {
+                    child.Visibility = Visibility.Visible;
+                }
+                Canvas.SetLeft(pacman, pacmanOriginalLeft);
+                Canvas.SetTop(pacman, pacmanOriginalTop);
+
+            }
+
+            // Reset Pac-Man's position (you can set it to a default starting position)
+            Canvas.SetLeft(pacman, 50); // Example starting position
+            Canvas.SetTop(pacman, 50);  // Example starting position
+
+            // Reset the game ended flag
+            isGameEnded = false;
+
+            // Start the enemy move timer again
+            enemyMoveTimer.Start();
         }
 
         private void StartGame_Click(object sender, RoutedEventArgs e)
@@ -106,5 +240,18 @@ namespace WpfAppTest
             // Focus on the canvas to allow key events
             MyCanvas.Focus();
         }
+        private void EndGame(string message)
+        {
+            enemyMoveTimer.Stop(); // Stop the enemies from moving
+            MessageBox.Show(message); // Display the message
+
+            // Hide the game canvas and show the start button
+            MyCanvas.Visibility = Visibility.Collapsed;
+            StartButton.Visibility = Visibility.Visible;
+            ResetGame(); // Reset the game state
+        }
+
+
+
     }
 }
